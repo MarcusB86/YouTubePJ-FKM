3224c02b66da8307a80c3b1705cfc9031daf9655
franklinTaylor <ftaylor07@icloud.com>
2024-05-11 17:12:56 -0400

    initial push

--- youtube-pj/src/Aboutme.jsx
+++ youtube-pj/src/Aboutme.jsx
@@ -10,18 +10,23 @@ export default function() {
             <h1>Kurt Julien</h1> <br />
             <p>Kurt, a native of Trinidad and Tobago, is a biking enthusiast who finds joy in exploring the island's scenic routes. Alongside his passion for the outdoors, Kurt is a proficient web developer, specializing in HTML, JavaScript, React, and CSS. When he's not immersed in code, you'll find him on the basketball court, fueled by his love for the game. An avid soca music enthusiast, Kurt brings a blend of creativity and skill to everything he pursues.
             </p>
+            <a href="https://github.com/kurtcoding">Github</a>
+
             </div>
             
             <div className="Marcus-Browne">
             <img src="https://ca.slack-edge.com/TCVA3PF24-U063SFA1DN3-1f2158372ace-512" alt="Marcus Browne" />
             <h1>Marcus Browne</h1> <br />
             <p>Marcus, a native of Montserrat, thrives on two passions: fitness and gaming. When he's not sculpting his physique in the gym, Marcus is mastering HTML, JavaScript, React, and CSS as a skilled web developer. With a competitive spirit and a drive for excellence, Marcus approaches each challenge with enthusiasm and determination, leaving his mark wherever he goes.</p>
+            <a href="https://github.com/MarcusB86">Github</a>
+
             </div>
 
             <div className="Franklin-Taylor">
             <img src="https://ca.slack-edge.com/TCVA3PF24-U0640BVN078-1482bd1bfd80-512" alt="Franklin-Taylor" />
             <h1>Franklin Taylor</h1> <br />
             <p>Franklin, from the island of Jamaica, is a devoted family man with a loving wife and two children. Outside his family commitments, he's a master of digital innovation, excelling in HTML, JavaScript, React, and CSS to craft seamless digital experiences. Embracing a pescatarian lifestyle, Franklin prioritizes health and sustainability in his diet. His unwavering commitment to excellence guides him as he tackles challenges with grit and ingenuity in every aspect of his life.</p>
+            <a href="https://github.com/Frankwhitee1">Github</a>
             </div>
 
         </div>
