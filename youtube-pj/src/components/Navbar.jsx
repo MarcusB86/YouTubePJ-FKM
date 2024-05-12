f516a293a6bf55f8317880f05544a84e58146a93
franklinTaylor <ftaylor07@icloud.com>
2024-05-12 16:18:50 -0400

    added search bar in nav

--- youtube-pj/src/components/Navbar.jsx
+++ youtube-pj/src/components/Navbar.jsx
@@ -6,9 +6,10 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
     return (
         <nav className='navbar'>
-            <p>Home</p>
-            <p>Favorites</p>
-            <p>About Me</p>
+            <Link to="/favorites">Favorites</Link>
+            <Link to="/home">Home</Link>
+            <Link to="/about">About Me</Link>
+            <input type="text" placeholder='Search video here' onChange={(e) => alert(e.target.value)} />
         </nav>
     );
 }
