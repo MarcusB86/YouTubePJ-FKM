7bcc6b0a93f7177dfd6509a2f0296492e7ae54ac
franklinTaylor <ftaylor07@icloud.com>
2024-05-12 16:37:26 -0400

    added input function

--- youtube-pj/src/components/Navbar.jsx
+++ youtube-pj/src/components/Navbar.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import './Navbar.css'
 
-
-export default function Navbar() {
+export default function Navbar({ handleSearchSubmit }) {
     return (
         <nav className='navbar'>
-            <img src="" alt="" />
             <Link to="/favorites">Favorites</Link>
             <Link to="/home">Home</Link>
-            <Link to="/Aboutme">About Me</Link>
+            <Link to="/about">About Me</Link>
+            <input type="text" placeholder='Search video here' onChange={(e) => handleSearchSubmit(e.target.value)} />
         </nav>
     );
-}
+} 
