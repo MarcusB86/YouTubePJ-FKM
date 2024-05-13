6e6517363b22b789c6d43781876cdfb125fac791
MarcusB86 <marcusb08@gmail.com>
2024-05-13 12:23:12 -0400

    Video_Update

--- youtube-pj/src/components/Video.jsx
+++ youtube-pj/src/components/Video.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+import Navbar from "./Navbar";
+
 export default function Video() {
+    const [videos, setVideos] = useState([])
+
 
     return (
         <div>
+
+            <iframe src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
+  frameborder="0" height="400" width="650" title="YoutubePlayer" >
+            </iframe>
             <p>Video Page</p>
         </div>
     )
