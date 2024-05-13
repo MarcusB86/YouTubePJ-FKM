78506a1ce931e3e85fdcab38c75f8ee9bb34cb36
MarcusB86 <marcusb08@gmail.com>
2024-05-13 12:48:32 -0400

    NavbarUpdate

--- youtube-pj/src/components/Navbar.jsx
+++ youtube-pj/src/components/Navbar.jsx
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom';
 // import './Navbar.css'
 
 
-export default function Navbar({ setSearchVideo}) {
+export default function Navbar({ handleSearchSubmit }) {
+
     return (
         <nav className='navbar'>
-            <p>Home</p>
-            <p>Favorites</p>
-            <p>About Me</p>
+            <Link to="/">Home</Link>
+            <Link to="/Aboutme">About Me</Link>
+            <input type="text" placeholder='Search video here' onChange={(e) => handleSearchSubmit(e.target.value)} />
         </nav>
     );
-}
+} 
