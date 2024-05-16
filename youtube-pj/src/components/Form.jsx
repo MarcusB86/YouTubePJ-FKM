bf21097f10d128b60f11c5c37cc1391c7ddb7476
kurtCoding <kjulien7790@gmail.com>
2024-05-15 22:06:58 -0400

    completed funtionality in both video and form.jsx

--- youtube-pj/src/components/Form.jsx
+++ youtube-pj/src/components/Form.jsx
@@ -2,29 +2,53 @@ import { useState } from "react";
 
 export default function Form() {
 
-    const [ comment, setComment ] = useState({
-        Name: "",
-        Comment: ""
+    const [ allComments, setAllComments ] = useState([]);
+    const [ addComment, setAddComment ] = useState({
+        name: "",
+        comment: ""
     });
+    // console.log(addComment.comment)
 
     function handleTextChange(e) {
-        setComment({
-            ...comment, 
+        setAddComment({
+            ...addComment, 
             [e.target.id]: e.target.value
         })
     }
-
+// console.log(addComment);
     function handleSubmit(event) {
         event.preventDefault();
-        setComment(comment);
+        setAllComments([
+            ...allComments, addComment
+        ])
+        console.log(allComments)
     }
 
     return (
-        <form className="commentForm" onChange={handleSubmit}>
-            <label > </label>
-            <input type="text" id="commenterName"/>
-            <input type="text" id="comment"/>
+        <>
+        <form className="commentForm" onSubmit={handleSubmit}>
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" onChange={handleTextChange} value={addComment.name}/> <br/>
+
+            <label htmlFor="comment">Comment:</label>
+            <input type="text" id="comment" onChange={handleTextChange} value={addComment.comment}/> <br/>
+
             <input type="submit" id="submitButton"/>
         </form>
+        <ul>
+            {allComments.map(comment => {
+                return (
+                    <>
+                        <li>
+                            {comment.name}
+                        </li>
+                        <li>
+                            {comment.comment}
+                        </li>
+                    </>
+                )
+            })}
+        </ul>
+        </>
     )
 }
